ae48dcfa48e6b45d34f3c42a1c14e2ba8b29c532
jebcode2021 <jebcode@outlook.com.br>
2023-12-02 23:00:38 -0300

    Desafio calculaArea

---
+++ EXERCICIO_JAVASCRIPT/calculaArea.js
@@ -0,0 +1,11 @@
+let base = prompt("Digite a base:");
+let altura = prompt("Digite a altura:");
+
+function calculaArea(base, altura) {
+    alert("A área do retângulo é: " + base * altura);
+}
+
+calculaArea(base, altura);
+
+
+
