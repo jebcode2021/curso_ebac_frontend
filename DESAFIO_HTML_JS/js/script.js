ac9621dd5928469d4f4a45786e254a64bf17fefa
jebcode2021 <jebcode@outlook.com.br>
2023-12-03 15:04:00 -0300

    formulário será válido e for inválido

---
+++ DESAFIO_HTML_JS/js/script.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('form-camposAB');
+const camposA = document.getElementById('camposA');
+const camposB = document.getElementById('camposB');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const mensagemCamposInvalidos = `O valor do campo A <b>${camposA.value}</b> é menor que o valor do campo B <b>${camposB.value}</b> são inválidos`;
+    const mensagemCamposValido = `O valor do campo B <b>${camposB.value}</b> é maior que o valor do campo A <b>${camposA.value}</b> são válidos`;
+
+   
+    const valorA = parseFloat(camposA.value);
+    const valorB = parseFloat(camposB.value);
+
+    if (valorB > valorA) {
+        const containerMensagemSucesso = document.querySelector('.success-message');
+        containerMensagemSucesso.innerHTML = mensagemCamposValido
+        containerMensagemSucesso.style.display = 'block';
+
+        camposA.value = '';
+        camposB.value = '';
+    } else {
+        const containerMensagemErro = document.querySelector('.success-message');
+        containerMensagemErro.innerHTML = mensagemCamposInvalidos;
+        containerMensagemErro.style.display = 'block';
+    }
+  
+    return false;
+});
+
+
+
+
+
+
+
+
+
